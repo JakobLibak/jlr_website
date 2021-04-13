8df69d4536d71431cc3483764842e6dc64a60448
Jakob Libak Rasmussen <jjakob@gmail.com>
2021-04-13 23:50:29 +0200

    add new subpage

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ def index():
     return render_template('index.html')
 
 @app.route('/about')
-def cakes():
+def about():
     return render_template('about.html')
 
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
+
+@app.route('/underside/<undersidenavn>')
+def underside(undersidenavn):
+    return render_template('underside.html', undersidenavn=undersidenavn)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
